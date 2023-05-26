0400c25ae5db5b1b2352bee02f06dd94d0e7699f
boney171 <ttran456@ucr.edu>
2023-05-25 22:57:06 -0700

    Finished setting up the enviroment

---
+++ my-backend/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3001;
+
+app.get('/api/data', (req, res) => {
+    res.json({message: 'Hello from server!'});
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
