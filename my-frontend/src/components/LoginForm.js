d9573206c21c06f0a043f8ed4399c6da2cc2b5b6
Howie315 <nguyen.howie2010@gmail.com>
2023-06-06 20:19:36 -0700

    Added new css for loggin

--- my-frontend/src/components/LoginForm.js
+++ my-frontend/src/components/LoginForm.js
@@ -1,74 +1,105 @@
-
-import './css/LoginForm.css'
-import RegistrationForm from './RegistrationForm';
-import {useState} from 'react';
-import axios from 'axios';
+import "./css/LoginForm.css";
+import RegistrationForm from "./RegistrationForm";
+import { useState } from "react";
+import imageSrc from "./images/wallpaper2.jpg";
+import axios from "axios";
 function LoginForm(props) {
-    const [loginModal, setLoginModal] = useState(true);
-    const [registerModal, setRegisterModal] = useState(false);
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    
-    //This function is to toggle login modal and register modal on and off
-    const handleRegisterClick = () => {
-      setLoginModal(false);
-      setRegisterModal(true);
-    }
-    
-    //This function handle login
-    const handleLoginClick = async (e) => {
-      e.preventDefault();
-    
-      //Create temp user object
-      const user = {
-        username: username,
-        password: password,
-      };
-    
+  const [loginModal, setLoginModal] = useState(true);
+  const [registerModal, setRegisterModal] = useState(false);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-      //Send an api call to the server to verify if user exists
-      try {
-        const response = await axios.post('http://localhost:3001/api/auth/login', user, {withCredentials: true});
-    
-        const data = response.data;
-        
-        //If server send back sessionID === null, means user does not exists
-        if(data.sessionID === null){
-          alert(data.message);
-          
-          //If server send back a legit sessionID, means user exists
-        } else {
-          props.setSessionID(data.user);
-          console.log(data.user);
-          //to retrieve const sessionID = localStorage.getItem('sessionID');
-          props.setSessionID(data.user);
-          props.onSessionChange();
-          setLoginModal(false);
-        }
-    
-      } catch (error) {
-        console.log(error);
-      }
+  //This function is to toggle login modal and register modal on and off
+  const handleRegisterClick = () => {
+    setLoginModal(false);
+    setRegisterModal(true);
+  };
+
+  //This function handle login
+  const handleLoginClick = async (e) => {
+    e.preventDefault();
+
+    //Create temp user object
+    const user = {
+      username: username,
+      password: password,
     };
 
-    return (
-      loginModal ? (
-        <div className="formContainer">
-          <form className="loginForm" onSubmit={handleLoginClick}>
-            <label className="up">Username</label>
-            <input type="text" value={username} onChange={e=> setUsername(e.target.value)} />
-    
-            <label className="up">Password</label>
-            <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
-    
-            <button type="button" onClick={handleRegisterClick}>Register</button>
-            <input type="submit" value="Login" />
-          </form>
-        </div>
-      ) : (
-        registerModal ? <RegistrationForm setLoginModal={setLoginModal} setRegisterModal={setRegisterModal} /> : null
-      )
-    );
+    //Send an api call to the server to verify if user exists
+    try {
+      const response = await axios.post(
+        "http://localhost:3001/api/auth/login",
+        user,
+        { withCredentials: true }
+      );
+
+      const data = response.data;
+
+      //If server send back sessionID === null, means user does not exists
+      if (data.sessionID === null) {
+        alert(data.message);
+
+        //If server send back a legit sessionID, means user exists
+      } else {
+        props.setSessionID(data.user);
+        console.log(data.user);
+        //to retrieve const sessionID = localStorage.getItem('sessionID');
+        props.setSessionID(data.user);
+        props.onSessionChange();
+        setLoginModal(false);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return loginModal ? (
+    <div className="container">
+      <div className="imageContainer">
+        <img src={imageSrc} alt="Your Image" className="image" />
+      </div>
+      <div className="formContainer">
+        <div className="title">Login</div>
+        <form className="loginForm" onSubmit={handleLoginClick}>
+          {/* <label className="up">Username</label> */}
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            placeholder="Username"
+          />
+
+          {/* <label className="up">Password</label> */}
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Password"
+          />
+
+          <input type="submit" value="Login" />
+          <p>
+            Don't have an account?{" "}
+            <span
+              onClick={handleRegisterClick}
+              style={{
+                color: "blue",
+                textDecoration: "underline",
+                cursor: "pointer",
+              }}
+            >
+              Click here
+            </span>
+          </p>
+        </form>
+      </div>
+    </div>
+  ) : registerModal ? (
+    <RegistrationForm
+      setLoginModal={setLoginModal}
+      setRegisterModal={setRegisterModal}
+    />
+  ) : null;
 }
 
 export default LoginForm;
